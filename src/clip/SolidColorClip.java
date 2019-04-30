0c6004683d4ba6ad2185a5764005f30260eeed2b
Aaron Wilson <aaron@earthromp.com>
2019-04-30 13:42:18 -0600

    Reverting failed websocket merge. Added Solid Color Clip.

---
+++ src/clip/SolidColorClip.java
@@ -0,0 +1,9 @@
+package clip;
+
+public class SolidColorClip extends AbstractClip{
+
+    //constructor
+    SolidColorClip(String theClipName, String theChannel) {
+        super(theClipName, theChannel);
+    }
+}
