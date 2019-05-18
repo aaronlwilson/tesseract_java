2cbff3e1bc09d3e8cf90bc64ac7047a6e2088bbc
Brandon Fryslie <530235+brandon-fryslie@users.noreply.github.com>
2019-05-18 00:36:44 -0600

    Implement playlist functionality

--- src/show/Playlist.groovy
+++ src/show/Playlist.groovy
@@ -1,19 +1,82 @@
-package show;
+package show
+
+import model.Channel
+import state.StateManager;
 
 public class Playlist {
   int id;
   String displayName;
+  Channel channel;
   Integer defaultDuration;
   List<PlaylistItem> items;
+
+  // The index of the current playlist item
+  private int currentIdx;
 
   public Playlist(int id, String displayName, Integer defaultDuration = 60, List<PlaylistItem> items = []) {
     this.id = id;
     this.displayName = displayName;
     this.defaultDuration = defaultDuration;
     this.items = items;
+    this.currentIdx = 0;
   }
 
+  public void setChannel(Channel channel) {
+    this.channel = channel;
+  }
 
+  // Plays a scene
+  public void playItem(PlaylistItem item) {
+    this.channel.setScene(item.scene, false, 10);
 
+    // Send a 'stateUpdated' event to the UI.  we will need to send one of these whenever state changes and we need to update the frontend
+    // for now this is a one-off that sets the activeScene on the 'controls' panel of the UI
 
+    // Send the active state to the client (e.g., current playlist and playlist item)
+    StateManager.get().sendStateUpdate("activeState", [
+        playlistItemId: item.id,
+        playlistId: this.id,
+    ]);
+
+    System.out.println("Playing scene '${item.scene.getDisplayName()}' on playlist '${this.displayName}'");
+  }
+
+  private void scheduleNextScene(long delay) {
+    TimerTask task = new TimerTask() {
+      public void run() {
+        play();
+
+        //System.out.println("Task performed on: " + new Date() + "n" + "Thread's name: " + Thread.currentThread().getName());
+      }
+    };
+
+    Timer timer = new Timer("Playlist.play()");
+    timer.schedule(task, delay);
+  }
+
+  public void play() {
+    this.play(true);
+  }
+
+  public void play(boolean shouldScheduleNextItem) {
+    PlaylistItem currentItem = this.items[this.currentIdx];
+    this.playItem(currentItem);
+
+    // Schedule the next item
+    if (shouldScheduleNextItem) {
+      PlaylistItem nextItem = this.items[this.currentIdx];
+      long delay = nextItem.duration * 1000 as long;
+      this.scheduleNextScene(delay);
+    }
+
+    // always repeat for now
+    this.currentIdx++;
+    if (this.currentIdx >= this.items.size()) {
+      this.currentIdx = 0;
+    }
+  }
+
+  public void stop() {
+    // here is where we would cancel the timer
+  }
 }
