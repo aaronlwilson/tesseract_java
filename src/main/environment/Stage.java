6c7f4a56e3ca2896c731885a374b679400d022dc
Aaron Wilson <aaron@earthromp.com>
2019-08-04 11:22:09 -0600

    mapping PixelPlanes starting LinesClip

--- src/main/environment/Stage.java
+++ src/main/environment/Stage.java
@@ -79,29 +79,30 @@ public class Stage {
         _myMain.udpModel.rabbits = new Rabbit[6];
 
         //one rabbit per 9 tiles
-        _myMain.udpModel.rabbits[0] = new Rabbit("192.168.1.101", 1, "mac_address");
-        _myMain.udpModel.rabbits[1] = new Rabbit("192.168.1.100", 2, "mac_address");
-        _myMain.udpModel.rabbits[2] = new Rabbit("192.168.1.102", 3, "mac_address");
-        _myMain.udpModel.rabbits[3] = new Rabbit("192.168.1.103", 4, "mac_address");
-        _myMain.udpModel.rabbits[4] = new Rabbit("192.168.1.105", 5, "mac_address");
-        _myMain.udpModel.rabbits[5] = new Rabbit("192.168.1.104", 6, "mac_address");
+        _myMain.udpModel.rabbits[0] = new Rabbit("192.168.1.100", 1, "mac_address");
+        _myMain.udpModel.rabbits[1] = new Rabbit("192.168.1.105", 2, "mac_address");
+        _myMain.udpModel.rabbits[2] = new Rabbit("192.168.1.104", 3, "mac_address");
+        _myMain.udpModel.rabbits[3] = new Rabbit("192.168.1.102", 4, "mac_address");
+        _myMain.udpModel.rabbits[4] = new Rabbit("192.168.1.101", 5, "mac_address");
+        _myMain.udpModel.rabbits[5] = new Rabbit("192.168.1.103", 6, "mac_address");
 
+        int startY = -72;
 
-        nodes = plane.buildPanel(_myMain.udpModel.rabbits[0], counter,0,-72,0, 0, false );
+        nodes = plane.buildPanel(_myMain.udpModel.rabbits[0], counter,-(72*9),startY,0, 0, false );
 
-        Node[] planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[1], counter,(72*3),-72,0, 0, false );
+        Node[] planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[1], counter,-(72*6),startY,0, 0, true );
         nodes = (Node[]) p.concat( nodes, planeNodes );
 
-        planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[2], counter,(72*6),-72,0, 0, false );
+        planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[2], counter,-(72*3),startY,0, 0, false );
         nodes = (Node[]) p.concat( nodes, planeNodes );
 
-        planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[3], counter,-(72*3),-72,0, 0, false  );
+        planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[3], counter,0,startY,0, 0, false  );
         nodes = (Node[]) p.concat( nodes, planeNodes );
 
-        planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[4], counter,-(72*6),-72,0, 0, true );
+        planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[4], counter,(72*3),startY,0, 0, false );
         nodes = (Node[]) p.concat( nodes, planeNodes );
 
-        planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[5], counter,-(72*9),-72,0, 0, false );
+        planeNodes = plane.buildPanel(_myMain.udpModel.rabbits[5], counter,(72*6),startY,0, 0, false );
         nodes = (Node[]) p.concat( nodes, planeNodes );
 
     }
@@ -121,19 +122,6 @@ public class Stage {
         _myMain.udpModel.teensies[2] = new Teensy("192.168.1.202", 3, "mac_address");
         _myMain.udpModel.teensies[3] = new Teensy("192.168.1.203", 4, "mac_address");
 
-
-        /*//OLD APOGAEA setup
-        nodes = new Node[h*w];
-
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                Node dracoNode = new Node(30 * i, 40 * j, 0, counter, null);
-                nodes[counter] = dracoNode;
-                _myMain.udpModel.teensies[i].nodeArray[j] = dracoNode;
-                counter++;
-            }
-        }
-        */
 
         StrandPanel panelOne = new StrandPanel(p);
         nodes = panelOne.buildPanel(teensyOne, 1, 1, counter, 0, 0, 0, 0);
